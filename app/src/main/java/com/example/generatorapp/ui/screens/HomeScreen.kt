973f04ac8d7fd4530883a592e42a93e835890dd8
ibrahimmoayed039-netizen <ibrahimmoayed039@gmail.com>
2026-08-30 22:38:59 +0300

    Add files via upload

--- app/src/main/java/com/example/generatorapp/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/example/generatorapp/ui/screens/HomeScreen.kt
@@ -19,6 +19,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(
     onOpenSubscribers: () -> Unit,
